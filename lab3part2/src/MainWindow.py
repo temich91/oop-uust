92fe1cd4ad6fc585d544da22327a6026b50372bd
temich91 <temich-ivanoff@yandex.ru>
2026-03-25 10:49:21 +0500

    часть разметки формы

--- lab3part2/src/MainWindow.py
+++ lab3part2/src/MainWindow.py
@@ -1,7 +1,22 @@
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLabel, QLineEdit, QSlider, QSpinBox
 from PyQt5.QtCore import QSize
+from lab3part2.src.Model import Model
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setFixedSize(QSize(700, 400))
+        mainWidget = QWidget(self)
+
+        self.model = Model()
+
+        mainLayout = QHBoxLayout()
+
+        self.aLayout = QVBoxLayout()
+        
+
+        mainLayout.addLayout(self.aLayout)
+        mainLayout.addLayout(self.bLayout)
+        mainLayout.addLayout(self.cLayout)
+        mainWidget.setLayout(mainLayout)
+        self.setCentralWidget(mainWidget)
