2bd1b7ef5a27de59d108c1654ae68e4f71fe9b7d
temich91 <temich-ivanoff@yandex.ru>
2026-04-19 18:55:04 +0500

    ЛР5: Работа с умными указателями

--- lab5/smart_pointers.cpp
+++ lab5/smart_pointers.cpp
@@ -5,23 +5,32 @@
 
 using namespace std;
 
+void pass_unique(unique_ptr<Desc> ptr) {
+    printf("unique_ptr используется в функции\n");
+}
 
-void smartPtrDemo() {
-    {
-        unique_ptr<Base> p1 = make_unique<Desc>();
-    } // удаление автоматически
-
-    {
-        shared_ptr<Base> p2 = make_shared<Desc>();
-        shared_ptr<Base> p3 = p2;
-
-        cout << "use_count = " << p2.use_count() << endl;
-    } // удалится когда счетчик = 0
+void pass_shared(shared_ptr<Desc> ptr) {
+    printf("shared_ptr используется в функции\n");
+    printf("Кол-во использований указателя при вызове функции : %d\n", ptr.use_count());
 }
 
 int main() {
     SetConsoleCP(65001);
     SetConsoleOutputCP(65001);
-    smartPtrDemo();
+    printf("unique_ptr\n");
+    
+    unique_ptr<Desc> un_desc = make_unique<Desc>();
+    // unique_ptr<Desc> second_unique = un_desc; // копирование unique_ptr запрещено
+    pass_unique(move(un_desc));
+    if (un_desc == nullptr) printf("Исходная переменная-указатель пуста\n");
+
+
+    printf("shared_ptr\n");
+    shared_ptr<Desc> sh_desc = make_shared<Desc>();
+    shared_ptr<Desc> second_shared = sh_desc; // копировать можно
+    printf("Кол-во использований исходного указателя: %d\n", sh_desc.use_count());
+    pass_shared(sh_desc);
+    printf("Кол-во использований исходного указателя после вызова функции: %d\n", sh_desc.use_count());
+    
     return 0;
 }
